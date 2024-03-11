04af8b5ebcc05c3d3036d3ed1e2f0b06658bcc01
Brain-Effects <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@github.coom>
2024-03-11 14:19:40 -0700

    Create BaseModel from dictionary

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@ import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 
+
 class TestBaseModel(unittest.TestCase):
     """
     Test the functionality of the BaseModel class
@@ -12,43 +13,55 @@ class TestBaseModel(unittest.TestCase):
         """
         Set up the tests
         """
-        self.model = BaseModel()
+        self.model1 = BaseModel()
+        self.model2 = BaseModel(**{
+            'id': '123',
+            'created_at': '2020-06-30T15:09:32.095238',
+            'updated_at': '2020-06-30T15:09:32.095238'})
 
     def test_id(self):
         """
         Test the id attribute
         """
-        self.assertEqual(type(self.model.id), str)
+        self.assertEqual(type(self.model1.id), str)
+        self.assertEqual(self.model2.id, '123')
 
     def test_created_at(self):
         """
         Test the created_at attribute
         """
-        self.assertEqual(type(self.model.created_at), datetime)
+        self.assertEqual(type(self.model1.created_at), datetime)
+        self.assertEqual(self.model2.created_at, datetime.strptime(
+            '2020-06-30T15:09:32.095238',
+            "%Y-%m-%dT%H:%M:%S.%f"))
 
     def test_updated_at(self):
         """
         Test the updated_at attribute
         """
-        self.assertEqual(type(self.model.updated_at), datetime)
+        self.assertEqual(type(self.model1.updated_at), datetime)
+        self.assertEqual(self.model2.updated_at, datetime.strptime(
+            '2020-06-30T15:09:32.095238',
+            "%Y-%m-%dT%H:%M:%S.%f"))
 
     def test_save(self):
         """
         Test the save method
         """
-        old_updated_at = self.model.updated_at
-        self.model.save()
-        self.assertNotEqual(self.model.updated_at, old_updated_at)
+        old_updated_at = self.model1.updated_at
+        self.model1.save()
+        self.assertNotEqual(self.model1.updated_at, old_updated_at)
 
     def test_to_dict(self):
         """
         Test the to_dict method
         """
-        model_dict = self.model.to_dict()
+        model_dict = self.model1.to_dict()
         self.assertEqual(type(model_dict), dict)
         self.assertEqual(model_dict['__class__'], 'BaseModel')
         self.assertEqual(type(model_dict['created_at']), str)
         self.assertEqual(type(model_dict['updated_at']), str)
+
 
 if __name__ == '__main__':
     unittest.main()
